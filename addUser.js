5db663ecfaf991a9de4faf12fb87fc0e1cde7c87
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-17 15:49:27 +0300

    Update addUser.js

--- addUser.js
+++ addUser.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'manageProperty')));
+app.use(express.static(path.join(__dirname, 'frontEnd')));
 
 function checkEmailExists(email) {
     return new Promise((resolve, reject) => {
