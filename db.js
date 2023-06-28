ba92babb8a2257b04902cf5c7a812b62a5c9e547
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-28 21:43:48 +0300

    changes to connect

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Ne17991999@',
+  password: 'Bujumbura1#',
   database: 'propertyproject',
 });
 
